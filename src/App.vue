de59da7780a07cfa2f0e64da0599c4db7f23431a
Mhmod-hsn <hsn.mhmod.96@gmail.com>
2020-04-14 11:40:07 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="sass">
+  @import "./assets/custom"
+  @import '../node_modules/bootstrap/scss/bootstrap'
+  @import '../node_modules/bootstrap-vue/src/index.scss'
+
+
+</style>
